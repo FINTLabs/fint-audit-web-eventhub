01426405fcf94026fbfbcdab6e8489c0625fa37e
Asgeir Nilsen <asgeir@fintlabs.no>
2020-02-27 08:30:11 +0100

    Fixed API controller media type and required path elements.

--- src/main/java/no/fint/audit/web/controller/ApiController.java
+++ src/main/java/no/fint/audit/web/controller/ApiController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import no.fint.audit.model.AuditEvent;
 import no.fint.audit.web.repository.EventsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -12,19 +13,19 @@ import java.util.function.Predicate;
 
 @CrossOrigin
 @RestController
-@RequestMapping(value = "/events/api")
+@RequestMapping(value = "/events/api", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 @Slf4j
 public class ApiController {
 
     @Autowired
     private EventsRepository eventsRepository;
 
-    @GetMapping({"{orgid}/{source:.+}", "{orgid}/{source}/{action:.+}", "{orgid}/{source}/{action}/{status:.+}"})
+    @GetMapping({"{orgid:.+}", "{orgid}/{source:.+}", "{orgid}/{source}/{action:.+}", "{orgid}/{source}/{action}/{status:.+}"})
     public List<AuditEvent> getEventsJson(
             @PathVariable String orgid,
             @PathVariable String source,
-            @PathVariable String action,
-            @PathVariable String status,
+            @PathVariable(required = false) String action,
+            @PathVariable(required = false) String status,
             @RequestParam(required = false, defaultValue = "20m") String period,
             @RequestParam(required = false, defaultValue = "1000") long limit
     ) {
