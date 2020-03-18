687c2ae3c080a0769d0eeda5c7fc235b6ab0324b
Asgeir Nilsen <asgeir@fintlabs.no>
2020-03-18 11:44:20 +0100

    Use worker pool instead of queue.

--- src/main/java/no/fint/audit/web/worker/EventsWorker.java
+++ src/main/java/no/fint/audit/web/worker/EventsWorker.java
@@ -5,48 +5,22 @@ import lombok.extern.slf4j.Slf4j;
 import no.fint.audit.web.repository.EventsRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @Service
 @Slf4j
 public class EventsWorker {
-    private final BlockingQueue<EventContext> queue;
     private final EventsRepository eventsRepository;
     private final Executor executor;
-    private final AtomicInteger threadCount;
 
     public EventsWorker(EventsRepository eventsRepository) {
         this.eventsRepository = eventsRepository;
-        queue = new LinkedBlockingQueue<>();
-        executor = Executors.newCachedThreadPool();
-        threadCount = new AtomicInteger();
+        executor = Executors.newWorkStealingPool();
     }
 
     public void add(EventContext eventContext) {
-        if (!queue.offer(eventContext)) {
-            eventsRepository.add(eventContext);
-        } else if (queue.size() > 10) {
-            start();
-        }
+        executor.execute(() -> eventsRepository.add(eventContext));
     }
 
-    public void start() {
-        executor.execute(this::work);
-    }
-
-    public void work() {
-        try {
-            threadCount.incrementAndGet();
-            EventContext eventContext = queue.poll();
-            while (eventContext != null) {
-                eventsRepository.add(eventContext);
-                eventContext = queue.poll(2500, TimeUnit.MILLISECONDS);
-            }
-        } catch (Exception e) {
-            log.warn(e.getMessage());
-        } finally {
-            threadCount.decrementAndGet();
-        }
-    }
 }
